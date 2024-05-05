b00033d07a9949f6cfe1aa22714ba23c491b28a4
Patrik Mäki <patrik.m.maki@gmail.com>
2024-05-05 17:14:34 +0300

    docker swarm does not seem to work when manager is from macos

--- 01_virtual_setup_manager.sh
+++ 01_virtual_setup_manager.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker swarm init --advertise-addr 10.211.55.2
+docker swarm init --advertise-addr 10.211.55.3
--- 02_virtual_setup_worker.sh
+++ 02_virtual_setup_worker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker swarm join --token SWMTKN-1-41iuhbc1zjvdm30tgbddu7uhrivqoe4g3jxg3piepyrmnerhwm-6hwi8x60038si74v58566wuuq 10.211.55.2:2377
+docker swarm join --token SWMTKN-1-1szgjrtcx5pci077jszfzyemholuywswlv2bfutdhs4focwg8b-150gjfnavde4goouvj41wy6b8 10.211.55.3:2377
+
