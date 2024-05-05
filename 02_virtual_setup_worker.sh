778ed5724bcca21046059ac82a566c4740e580b4
Patrik Mäki <patrik.m.maki@gmail.com>
2024-05-05 17:41:16 +0300

    wip

--- 02_virtual_setup_worker.sh
+++ 02_virtual_setup_worker.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-docker swarm join --token SWMTKN-1-1szgjrtcx5pci077jszfzyemholuywswlv2bfutdhs4focwg8b-150gjfnavde4goouvj41wy6b8 10.211.55.3:2377
-
+docker swarm join --token SWMTKN-1-4b4794azyggpi5rfpbsf5jptwkkpd4zd61xa621j6n214y6ilq-3gsvyj43mjk8qd551vb5f8x3g 10.211.55.3:2377
